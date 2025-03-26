aa806bd2fd0b1c19cceb91300bf74aab9829245c
Roberto de la Cámara <robcamgar@outlook.es>
2025-03-26 19:47:28 +0100

    Enhance setup.py with testing dependencies and entry points for console scripts

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ml-ids",  # Nombre del paquete
+    name="ml-ids",  
     version="0.1",
-    packages=find_packages(where="src"),  # Busca paquetes dentro de 'src'
-    package_dir={"": "src"},  # Indica que los paquetes están en 'src'
-    install_requires=[],  # Aquí puedes incluir dependencias si es necesario
+    packages=find_packages(where="src"),  # Loops through 'src' to find packages
+    package_dir={"": "src"},  # Indicates that the root package is in 'src'
+    install_requires=[],  # Here you can add the dependencies
+    extras_require={"test": ["pytest", "pytest-cov", "requests-mock"]},  # Dependencies for testing
+    tests_require=["pytest", "pytest-cov", "requests-mock"],  # Dependencies for testing
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "train-model=src.train_model:main",
+            "train-server=src.train_server:main"
+        ]
+    }
 )
