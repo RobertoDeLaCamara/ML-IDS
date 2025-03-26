34442899c71e0976092bb2e51705029a7b7e1d6e
Roberto de la Cámara <robcamgar@outlook.es>
2025-03-26 19:41:51 +0100

    Add initial setup.py for package configuration and dependencies

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ML-IDS",  # Nombre del paquete
+    version="0.1",
+    packages=find_packages(where="src"),  # Busca paquetes dentro de 'src'
+    package_dir={"": "src"},  # Indica que los paquetes están en 'src'
+    install_requires=[],  # Aquí puedes incluir dependencias si es necesario
+)
