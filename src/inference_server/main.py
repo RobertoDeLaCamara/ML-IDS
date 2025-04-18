6c92c1e8f0bbb84cf238a39e756e79c5e4183a77
Roberto de la Cámara <robcamgar@outlook.es>
2025-04-18 17:58:52 +0200

    Refactor inference server; enhance model initialization and error handling, update tests for response status codes

--- src/inference_server/main.py
+++ src/inference_server/main.py
@@ -1,40 +1,37 @@
-from fastapi import FastAPI
-import mlflow
+from fastapi import FastAPI, HTTPException
 import pandas as pd
 import os
 
 app = FastAPI()
 
-def init_model():
-    """
-    Initializes the machine learning model by setting the necessary
-    environment variables for MLflow S3 endpoint and AWS credentials.
-    Loads the registered model 'CICD_IDS_Model_v1' in production stage
-    from MLflow Model Registry and assigns it to a global variable.
-    """
+model_initialized = False
 
+
+def init_model():
+    global model, model_initialized
+    import mlflow
+    from mlflow.exceptions import MlflowException
     os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://192.168.1.189:9000"
     os.environ["AWS_ACCESS_KEY_ID"] = "roberto"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "patilla1"
-    global model
-    model = mlflow.sklearn.load_model("models:/CICD_IDS_Model_v1/Production")
-
-init_model()
+    try:
+        model = mlflow.sklearn.load_model("models:/CICD_IDS_Model_v1/Production")
+        model_initialized = True
+    except MlflowException as e:
+        model = None
+        model_initialized = False
+        raise HTTPException(status_code=503, detail=f"Model not available: {str(e)}")
 
 @app.post("/predict")
 def predict(features: dict):
-    """
-    Receives a dictionary of features, converts it into a DataFrame,
-    and returns a prediction from the pre-loaded machine learning model.
-
-    Parameters:
-    features (dict): A dictionary containing feature names as keys and 
-                     their corresponding values as values.
-
-    Returns:
-    dict: A dictionary containing the prediction result in a list form.
-    """
-
+    global model_initialized, model
+    if not model_initialized:
+        try:
+            init_model()
+        except HTTPException as e:
+            raise e
+    if model is None:
+        raise HTTPException(status_code=503, detail="Model not available.")
     df = pd.DataFrame([features])
     prediction = model.predict(df)
     return {"prediction": prediction.tolist()}
